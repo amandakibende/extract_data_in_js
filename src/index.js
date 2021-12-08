17877079abb01ef09ab5f5325ecf92c37abcbce2
Amanda <61988960+amandakibende@users.noreply.github.com>
2021-12-08 02:18:16 +0100

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
- in js import "./styles.css";
+ /*in js import "./styles.css";*/
 
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
@@ -8,3 +8,36 @@ document.getElementById("app").innerHTML = `
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
+
+/* function showPrice() {
+  console.log("price here!");
+}
+document.addEventListener("mouseover", showPrice);
+
+function showProd(){
+  console.log("products!!!!!");
+}
+let products = document.querySelectorAll('.ProductCard');
+console.log(products);
+
+products.addEventListener("mouseover", showProd);
+
+
+function showProd(){
+  console.log("rowlin!!!!!");
+}
+let products = document.querySelector('.ProductList.ProductList--grid');
+console.log(products);
+
+products.addEventListener("mouseover", showProd);
+
+let products = document.querySelector('.ProductList.ProductList--grid');
+console.log(products);
+products.addEventListener("mouseover", function(e){
+console.log("here :", e.target.tagName);
+});
+
+let price = document.getElementsByClassName('ProductCard')[i].getElementsByTagName('strong')[0].innerHTML;
+let name = document.getElementsByClassName('ProductCard')[i].innerHTML;
+
+*/
